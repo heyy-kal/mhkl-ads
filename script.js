3e428e04e23933faee852bbfec5171b8472555ef
heyy-kal <14haikal@gmail.com>
2024-12-10 20:08:03 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("showAd").addEventListener("click", function () {
-  // Iklan akan dimuat ketika tombol diklik
-  const adScript = document.createElement("script");
-  adScript.src = "//niphaumeenses.net/vignette.min.js";
-  adScript.setAttribute("data-zone", "8621206");
-  adScript.setAttribute("data-sdk", "show_8621206");
-  document.body.appendChild(adScript);
-  alert("Iklan sedang dimuat...");
-});
